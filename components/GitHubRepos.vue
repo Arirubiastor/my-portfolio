30879a60a125280dff4167132ed9a823e4c0b17b
Ariana Rubi <arianarubigo@gmail.com>
2021-01-04 12:55:24 +0100

    github repositories section

---
+++ components/GitHubRepos.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <!-- Header title -->
+    <header class="pt-4">
+      <h2 class="font-bold text-lg md:text-2xl">Github Repositories</h2>
+    </header>
+    <!-- Grid mobile: 1, tablet: 2, desktop: 3 columns -->
+    <div
+      v-if="repositoriesData && repositoriesData.length > 0"
+      class="flex flex-wrap pt-4 md:-mr-6"
+    >
+      <!-- <div
+        v-for="repo in repositoriesData"
+        :key="repo.id"
+        class="w-full md:w-1/2 lg:w-1/3 md:pr-6 pb-4"
+      >
+        <GitHubReposItem
+          :repository="repositoriesData"
+          :bg-color="repo.language ? bgColorsData[repo.language].color : '#ffffff'"
+          v-for="repo in repositoriesData"
+        :key="repo.id"
+        />
+      </div> -->
+      <b-card-group columns>
+      <GitHubReposItem
+        :repositories="repositoriesData"
+        :bg-color="bgColorsData"
+      />
+      </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import GitHubReposItem from "~/components/GitHubReposItem.vue";
+
+export default {
+  name: "GitHubRepos",
+  components: {
+    GitHubReposItem,
+  },
+  props: {
+    repositoriesData: {
+      type: Array,
+      required: true,
+    },
+    bgColorsData: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,54 @@
 <template>
   <div>
     <TheHeader />
+    <GitHubRepos
+      :repositoriesData="repos"
+      :bgColorsData="colors"
+    />
   </div>
 </template>
 
 <script>
+import GitHubRepos from "~/components/GitHubRepos.vue";
+import axios from "axios";
+
 export default {
-  
-}
+  data() {
+    return {
+      repos: [],
+      colors: {},
+    };
+  },
+  components: {
+    GitHubRepos,
+  },
+  methods: {
+    getColors() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json"
+        )
+        .then((res) => (this.colors = res.data));
+    },
+    getRepos() {
+      axios
+        .get("https://api.github.com/users/Arirubiastor/repos")
+        .then((res) => {
+          this.repos = res.data
+            .filter((repo) => !repo.fork)
+            .sort(
+              (repo1, repo2) => repo2.stargazers_count - repo1.stargazers_count
+            )
+            .slice(0, 6);
+        });
+    },
+  },
+  async mounted() {
+    await this.getColors();
+    this.getRepos();
+  },
+};
 </script>
 
 <style lang="scss">
-
 </style>
