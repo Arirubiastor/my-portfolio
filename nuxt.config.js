acfb069c098f0771942b1a1ee61c0ae5a08e68f8
Ariana Rubi <arianarubigo@gmail.com>
2021-04-12 16:38:38 +0200

    perspective icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+      { rel: 'icon', type: 'image/x-icon', href: '/perspective.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap&effect=outline' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bungee+Hairline&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet' },
